ee96a07c7f95c220266ff5326f9b21dc21c07233
Tolga Nacar <83028055+tolganacar@users.noreply.github.com>
2024-03-19 19:22:20 +0300

    Add bottom nav bar

--- Gelivery/app/src/main/java/com/tolganacar/foodorderapp/MainActivity.kt
+++ Gelivery/app/src/main/java/com/tolganacar/foodorderapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.tolganacar.foodorderapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.tolganacar.foodorderapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navHostFragment.navController)
     }
 }
