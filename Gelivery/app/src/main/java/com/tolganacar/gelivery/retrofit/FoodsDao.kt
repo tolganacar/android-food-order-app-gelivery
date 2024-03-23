b15186ebfe37c39562716215bfd9b9f2080f7f55
Tolga Nacar <83028055+tolganacar@users.noreply.github.com>
2024-03-23 20:57:54 +0300

    Add cart page

--- Gelivery/app/src/main/java/com/tolganacar/gelivery/retrofit/FoodsDao.kt
+++ Gelivery/app/src/main/java/com/tolganacar/gelivery/retrofit/FoodsDao.kt
@@ -1,6 +1,7 @@
 package com.tolganacar.gelivery.retrofit
 
 import com.tolganacar.gelivery.data.entity.CRUDResponse
+import com.tolganacar.gelivery.data.entity.CartResponse
 import com.tolganacar.gelivery.data.entity.FoodsResponse
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
@@ -19,4 +20,13 @@ interface FoodsDao {
                           @Field("yemek_fiyat") yemek_fiyat: Int,
                           @Field("yemek_siparis_adet") yemek_siparis_adet: Int,
                           @Field("kullanici_adi") kullanici_adi: String) : CRUDResponse
+
+    @POST("yemekler/sepettekiYemekleriGetir.php")
+    @FormUrlEncoded
+    suspend fun getCartFoods(@Field("kullanici_adi") kullanici_adi: String) : CartResponse
+
+    @POST("yemekler/sepettenYemekSil.php")
+    @FormUrlEncoded
+    suspend fun deleteFood(@Field("sepet_yemek_id") sepet_yemek_id: Int,
+                           @Field("kullanici_adi") kullanici_adi: String) : CRUDResponse
 }
--- Gelivery/app/src/main/java/com/tolganacar/gelivery/ui/fragments/CartFragment.kt
+++ Gelivery/app/src/main/java/com/tolganacar/gelivery/ui/fragments/CartFragment.kt
@@ -1,24 +1,85 @@
 package com.tolganacar.gelivery.ui.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.tolganacar.gelivery.R
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import com.tolganacar.gelivery.databinding.FragmentCartBinding
+import com.tolganacar.gelivery.ui.adapter.CartAdapter
+import com.tolganacar.gelivery.ui.viewmodel.CartViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CartFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var binding: FragmentCartBinding
+    private lateinit var viewModel: CartViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+        binding = FragmentCartBinding.inflate(inflater, container, false)
+
+        viewModel.foodCartList.observe(viewLifecycleOwner) {
+            val cartAdapter = CartAdapter(requireContext(), it, viewModel)
+            binding.recyclerViewCart.adapter = cartAdapter
+            binding.recyclerViewCart.layoutManager = LinearLayoutManager(requireContext())
+        }
+
+        viewModel.totalPrice.observe(viewLifecycleOwner) {
+            binding.textViewSubtotalPrice.text = it.toString()
+            if (binding.textViewSubtotalPrice.text.equals("0")) {
+                binding.textViewTotalPriceCart.text = "0"
+            } else {
+                binding.textViewTotalPriceCart.text = (it + 10).toString()
+            }
+        }
+
+        binding.buttonConfirmCart.setOnClickListener {
+            if (!viewModel.foodCartList.value.isNullOrEmpty()) {
+                Snackbar.make(it, "Confirm your order?", Snackbar.LENGTH_LONG)
+                    .setAction("YES") {
+                        viewModel.deleteAllFoods("nacar")
+                        Snackbar.make(it, "Your order has been confirmed.", Snackbar.LENGTH_SHORT)
+                            .setBackgroundTint(Color.YELLOW)
+                            .setTextColor(Color.BLACK)
+                            .show()
+                    }
+                    .setBackgroundTint(Color.YELLOW)
+                    .setTextColor(Color.BLACK)
+                    .setActionTextColor(Color.RED)
+                    .show()
+            } else {
+                Snackbar.make(
+                    requireView(),
+                    "Please add items to your cart!",
+                    Snackbar.LENGTH_SHORT
+                )
+                    .setTextColor(Color.RED)
+                    .setBackgroundTint(Color.WHITE)
+                    .show()
+            }
+
+        }
+
+        viewModel.getCartFoods("nacar")
+
+        return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val tempViewModel: CartViewModel by viewModels()
+        viewModel = tempViewModel
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.getCartFoods("nacar")
     }
 }
